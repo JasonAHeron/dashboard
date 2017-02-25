8c08715c1e1ec10fcc90189bd8677d8876fea591
Jason Heron <jason@heron.io>
2017-02-24 18:13:40 -0800

    auth working

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, render_template
 from datetime import datetime
 from models import DeviceConnection
+from google.appengine.api import users
+
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
@@ -14,6 +16,8 @@ DEVICE_OWNERS = {
     'raspberrypi': 'House',
     '0005CD626483': 'House',
 }
+
+VALID_USERS = ['jasonaheron@gmail.com', 'michael@stewart.io']
 
 
 class DeviceInfo(object):
@@ -35,12 +39,42 @@ class DeviceInfo(object):
         return self.last_seen.seconds // 3600
 
 
+class DeviceOwner(object):
+    def __init__(self, name, current_time=datetime.now()):
+        self.devices = []
+        self.name = name
+        self.current_time = current_time
+
+    def add_device(self, device_connection):
+        self.devices.append(DeviceInfo(device_connection, self.current_time))
+
+    @property
+    def is_home(self):
+        return bool([device for device in self.devices if device.is_home])
+
+
 @app.route('/')
 def homepage():
     """Return the dashboard homepage."""
+    user = users.get_current_user()
+    if user.email() not in VALID_USERS:
+        login_url = users.create_login_url('/')
+        return render_template('home.html', login_url=login_url)
+
     device_connections = DeviceConnection.query()
     now = datetime.now()
-    return render_template('home.html', data=[DeviceInfo(connection, now) for connection in device_connections])
+
+    owners = {
+        'Jason': DeviceOwner('Jason', now),
+        'Michael': DeviceOwner('Michael', now),
+        'House': DeviceOwner('House', now),
+        'Other': DeviceOwner('Other', now)
+    }
+
+    for device_connection in device_connections:
+        owners[DEVICE_OWNERS.get(device_connection.device_name, 'Other')].add_device(device_connection)
+
+    return render_template('home.html', user=user, data=[owners['Jason'], owners['Michael'], owners['House'], owners['Other']])
 
 
 @app.route('/arp')
@@ -54,7 +88,7 @@ def update_devices():
                 DeviceConnection.update_time(key[0])
             else:
                 DeviceConnection.create(device)
-    return 'Updated {}'.format(devices.split('\n'))
+    return 'Updated {}'.format(devices.split('\n') if devices else 'NONE')
 
 
 @app.errorhandler(404)
