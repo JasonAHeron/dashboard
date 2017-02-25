a3cb4d3ea3f37d9c0c9bc38af7985dae0969b524
Jason Heron <jason@heron.io>
2017-02-24 23:42:07 -0800

    kinda broken

--- main.py
+++ main.py
@@ -16,15 +16,16 @@ DEVICE_OWNERS = {
     'raspberrypi': 'House',
     '0005CD626483': 'House',
 }
-
+PHONES = ['android-d8973f71e80d2c9b', 'Michaels-iPhone']
 VALID_USERS = ['jasonaheron@gmail.com', 'michael@stewart.io']
 
 
-class DeviceInfo(object):
+class Device(object):
+    """A device on the network."""
+
     def __init__(self, device_connection, current_time=datetime.now()):
         self.last_seen = current_time - device_connection.time
-        self.device_name = device_connection.device_name
-        self.owner = DEVICE_OWNERS.get(self.device_name, 'Unknown')
+        self.name = device_connection.device_name
 
     @property
     def is_home(self):
@@ -38,19 +39,25 @@ class DeviceInfo(object):
     def last_seen_hours(self):
         return self.last_seen.seconds // 3600
 
+    @property
+    def is_phone(self):
+        return self.name in PHONES
 
-class DeviceOwner(object):
+
+class Owner(object):
+    """An owner of one or more devices on the network."""
+
     def __init__(self, name, current_time=datetime.now()):
         self.devices = []
         self.name = name
         self.current_time = current_time
 
     def add_device(self, device_connection):
-        self.devices.append(DeviceInfo(device_connection, self.current_time))
+        self.devices.append(Device(device_connection, self.current_time))
 
     @property
     def is_home(self):
-        return bool([device for device in self.devices if device.is_home])
+        return bool([device for device in self.devices if device.is_home and device.is_phone])
 
 
 @app.route('/')
@@ -65,16 +72,23 @@ def homepage():
     now = datetime.now()
 
     owners = {
-        'Jason': DeviceOwner('Jason', now),
-        'Michael': DeviceOwner('Michael', now),
-        'House': DeviceOwner('House', now),
-        'Other': DeviceOwner('Other', now)
+        'Jason': Owner('Jason', now),
+        'Michael': Owner('Michael', now),
+        'House': Owner('House', now),
+        'Other': Owner('Other', now)
     }
 
-    for device_connection in device_connections:
-        owners[DEVICE_OWNERS.get(device_connection.device_name, 'Other')].add_device(device_connection)
+    owners = {}
 
-    return render_template('home.html', user=user, data=[owners['Jason'], owners['Michael'], owners['House'], owners['Other']])
+    for device_connection in device_connections:
+        owner_name = DEVICE_OWNERS.get(device_connection.device_name, 'Other')
+        if owners.get(owner_name):
+            owners[owner_name].add_device(device_connection)
+        else:
+            owners[owner_name] = Owner(owner_name, now)
+            owners[owner_name].add_device(device_connection)
+
+    return render_template('home.html', user=user, data=sorted(owners.values(), key=lambda x: x.name, reverse=True))
 
 
 @app.route('/arp')
